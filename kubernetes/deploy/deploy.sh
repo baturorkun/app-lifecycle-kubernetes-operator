077df4ed97e483b079a9f05086db85c4281668e2
Batur Orkun <batur.orkun@brado.>
2025-12-24 14:04:47 +0300

    ci: add release workflow for tagged builds and enhance deploy script with version tagging

--- kubernetes/deploy/deploy.sh
+++ kubernetes/deploy/deploy.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-export IMG=ghcr.io/baturorkun/app-lifecycle-kubernetes-operator:latest
+if [[ $1 == "" ]]; then
+    echo "Error: Version is missing. The version must be like that v0.1.0"
+    exit
+fi
+
+export IMG=ghcr.io/baturorkun/app-lifecycle-kubernetes-operator:$1
 
 make deploy IMG=$IMG
 
 kustomize build config/default > dist/install.yaml
+
+git tag "$1"
+
+git push origin "$1"
